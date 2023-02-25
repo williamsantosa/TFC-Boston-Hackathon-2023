839bf106ec5defe8bca8c39793ed674fd3c6d1ac
William Santosa <93417386+williamsantosa@users.noreply.github.com>
2023-02-25 12:35:27 -0500

    starting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  CssBaseline, Box, AppBar, Toolbar, IconButton,
+  Typography, Avatar, List,
+  ListItem, ListItemText, ListItemIcon, ListItemButton, Dialog,
+  Slide, Stack, Drawer, Divider,
+} from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <CssBaseline/>
+      <Box>
+        <AppBar>
+          <Typography>Hello</Typography>
+        </AppBar>
+      </Box>
+    </>
+  );
+}
+
+export default App;
