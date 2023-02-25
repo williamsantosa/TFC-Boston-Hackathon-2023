5af79a2d7370ee19bbbf2817fdad23af310792b1
William Santosa <93417386+williamsantosa@users.noreply.github.com>
2023-02-25 13:19:28 -0500

    updated, file works; git push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,49 @@
 import {
-  CssBaseline, Box, AppBar, Toolbar, IconButton,
-  Typography, Avatar, List,
-  ListItem, ListItemText, ListItemIcon, ListItemButton, Dialog,
-  Slide, Stack, Drawer, Divider,
+  CssBaseline, Box 
 } from '@mui/material';
 import React from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const onChange = (e) => {
+    let files = e.target.files;
+    console.warn('data file', files);
+
+    let reader = new FileReader();
+    reader.readAsText(files[0]);
+
+    reader.onload = async (e) => {
+      console.warn('img data', e.target.result);
+      
+      const parts = [
+        new Blob(['you construct a file...'], {
+          type: 'text/plain'
+        }),
+        ' Same way as you do with blob',
+        new Uint16Array([33])
+      ];
+      const file = new File(parts, 'sample.txt', {
+        lastModified: new Date(2020, 1, 1),
+        type: "text/plain"
+      });
+      const fr = new FileReader();
+      fr.onload = (evt) => {
+        document.body.innerHTML = `
+         <a href="${URL.createObjectURL(file)}" download="${file.name}">download</a>
+          <p>file type: ${file.type}</p>
+          <p>file last modified: ${new Date(file.lastModified)}</p>
+        `
+      }
+      fr.readAsText(file);
+    };
+  };
+
   return (
     <>
       <CssBaseline/>
       <Box>
-        <AppBar>
-          <Typography>Hello</Typography>
-        </AppBar>
+        <input type="file" name="file" onChange={e => onChange(e)}/>
       </Box>
     </>
   );
