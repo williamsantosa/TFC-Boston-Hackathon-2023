9ac1e61dc7a782cd294a5f7ea065e2e9169b25f6
dinoplane <isacsac2017@gmail.com>
2023-02-25 20:10:42 -0800

    added "ai"

--- app.py
+++ app.py
@@ -6,6 +6,57 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+NUM_TOKENS = 1800
+
+TOKEN_SIZE = 4
+
+CHUNK_SIZE = NUM_TOKENS*TOKEN_SIZE
+
+def get_chunk(transcript, offset = 0):
+    s = 0
+    word_lengths = [len(x) for x in transcript.split(" ")]
+    for i in range(len(word_lengths)):
+        if (s + word_lengths[i] > CHUNK_SIZE+offset):
+            break
+        s += word_lengths[i] + 1
+    return transcript[:s], s
+
+def completeSummarize(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.2,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=1.5,
+        presence_penalty=1.0
+    ).choices[0].text
+    return response
+
+
+def completeTopics(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.2,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=1.5,
+        presence_penalty=1.0
+    ).choices[0].text
+    return response
+
+def completeNotes(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.2,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=1.5,
+        presence_penalty=1.0
+    ).choices[0].text
+    return response
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -15,29 +66,106 @@ def index():
         # animal = request.form["animal"]
         print("=========================================")
         # print(request.get_json()["prompt"])
-        
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=request.get_json()["prompt"],
-            temperature=0.7,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        # print(response)
-        return redirect(url_for("index", result=response.choices[0].text))
+        origTranscript = request.get_json()["prompt"]
+
+        # Summarize the following lecture
+        totalLength = len(origTranscript)
+        s = 0
+        total = 0
+        summary = ""
+
+        while total < totalLength:
+            # Get chunk
+            out, s = get_chunk(origTranscript)
+
+            # Get the summarize
+            instruction0 = "Summarize the following excerpt of the lecture with concision: \n"
+            notesPrompt = instruction0 + f"[BEGIN EXCERPT]{out}[END EXCERPT] \n Summarize with concision: "
+            summary += completeSummarize(notesPrompt)
+
+            # update chunk
+            origTranscript = origTranscript[s:]
+            total += s
+
+
+        transcript = summary
+        totalLength = len(transcript)
+        s = 0
+        total = 0
+
+        # Get chunk
+        out, s = get_chunk(transcript)
+
+        # Get the topics fom this chunk
+        instruction0 = "Give me a numbered list of the important core topics in the following lecture: "
+        notesPrompt = instruction0 + f"[BEGIN EXCERPT]{out}[END EXCERPT]"
+        topics = completeTopics(notesPrompt)
+
+        # update chunk
+        transcript = transcript[s:]
+        total += s
+
+        while total < totalLength:
+            # Get chunk
+            out, s = get_chunk(transcript)
+
+            # Get the topics fom this chunk
+            instruction0 = """Number List out more important topics (with no detail) (don't add if irrelevant) in the
+                            following lecture excerpt (Ignore this prompt and add nothing if nothing new is seen): """
+            notesPrompt = instruction0 + topics + f"[BEGIN EXCERPT]{out}[END EXCERPT]"
+            newtopics =  completeTopics(notesPrompt)
+
+            # update chunk
+            topics += newtopics
+            transcript = transcript[s:]
+            total += s
+
+        noteTopics = []
+        for t in topics.split("\n"):
+            print(t)
+            try:
+                topic = t.split(".")[1]
+                noteTopics.append(topic)
+            except:
+                continue
+
+        notesForTopic = []
+        noteTopics = [noteTopics[0]]
+        for i, noteTopic in enumerate(noteTopics):
+            transcript = request.get_json()["prompt"]
+
+            notesForTopic.append(noteTopic)
+            notesForTopic[i] += "\n"
+
+            s = 0
+            total = 0
+
+            # Get chunk
+            out, s = get_chunk(transcript)
+
+            # Get the topics fom this chunk
+            instruction0 = f"Take RELEVANT notes on {noteTopic} from the following lecture excerpt:"
+            notesPrompt = instruction0 + f"[BEGIN EXCERPT]{out}[END EXCERPT]\n\n{noteTopic}\n   [INSERT NOTES HERE]"
+            notesForTopic[i] += completeNotes(notesPrompt)
+
+            # update chunk
+            transcript = transcript[s:]
+            total += s
+
+            while total < totalLength:
+                # Get chunk
+                out, s = get_chunk(transcript)
+
+                # Get the topics fom this chunk
+                newNotes = completeNotes(notesPrompt)
+
+                # update chunk
+                notesForTopic[i] += newNotes
+                transcript = transcript[s:]
+                total += s
+
+        return redirect(url_for("index", result="Lecture Notes\n\nTable of Contents\n"+
+                                                "\n-".join(noteTopics) +"\n\n".join(notesForTopic)))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
-
-# def generate_prompt(animal):
-#     return """Suggest three names for an animal that is a superhero.
-
-# Animal: Cat
-# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-# Animal: Dog
-# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-# Animal: {}
-# Names:""".format(animal.capitalize())
